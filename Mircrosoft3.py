164988b168ac129dfab6a9183f4fe080e45f7936
Siddhantv1 <siddhaaaaaaant@gmail.com>
2025-01-02 16:06:45 +0530

    solution MS-3

--- Mircrosoft3.py
+++ Mircrosoft3.py
@@ -3,4 +3,9 @@
 #find winner of Circular Game
 class Solution(object):
     def findTheWinner(self, n, k):
-        
+        array = [i for i in range(1, n+1)]
+        t = 0
+        while len(array) >1:
+            t = (t + (k-1)) % len(array)
+            array.pop(t)
+        return array[0]
