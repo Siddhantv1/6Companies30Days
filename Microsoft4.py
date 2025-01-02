4aa104ad9f18bc96cb93742d11318d4bbf23b185
Siddhantv1 <siddhaaaaaaant@gmail.com>
2025-01-02 17:07:18 +0530

    solution MS-4

--- Microsoft4.py
+++ Microsoft4.py
@@ -2,4 +2,19 @@
 class Solution(object):
     def minMoves2(self, nums):
         nums.sort()
-        
+        n = len(nums)
+
+        #find median
+        x = 0
+        if n%2 == 0:
+            x = (nums[n//2 -1] + nums[n//2]) / 2
+        else:
+            x = nums[n//2]
+
+
+        median = x
+        moves = 0
+        for i in nums:
+            moves += abs(i - median)
+        return moves
+
