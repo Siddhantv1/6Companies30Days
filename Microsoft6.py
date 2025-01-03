970955aea4e14b702afd3c5d0651d13bd24162d0
Siddhantv1 <siddhaaaaaaant@gmail.com>
2025-01-03 22:11:35 +0530

    solution MS-6

--- Microsoft6.py
+++ Microsoft6.py
@@ -1,8 +1,25 @@
 class Solution(object):
     def getHint(self, secret, guess):
-        """
-        :type secret: str
-        :type guess: str
-        :rtype: str
-        """
+        x = len(secret)
+        BullCount = 0 #counter for Bulls
+        CowCount = 0 #counter for Cows
+
+        #convert both strings to arrays for mutability
+        SecretArray = list(secret) 
+        GuessArray = list(guess)
         
+        for i in range(x):
+            if SecretArray[i] == GuessArray[i]:
+                BullCount +=1
+                #mark that element as counted
+                SecretArray[i] ='X'
+                GuessArray[i] ='X'
+
+        
+        for i in range(x):
+            if GuessArray[i] != 'X' and GuessArray[i] in SecretArray:
+                CowCount +=1
+                thatIndex = SecretArray.index(GuessArray[i])
+                SecretArray[thatIndex] = 'X'
+                GuessArray[i] = 'X'
+        return str(BullCount)+"A"+str(CowCount)+"B"
